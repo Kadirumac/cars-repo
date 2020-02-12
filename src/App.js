0251d9ef7712b54a5f9653d8e3ad93d98da93f8b
kadirumac <kadirumac49@gmail.com>
2020-02-12 11:47:50 +0100

    sad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
-
+import {Form,Button} from 'react-bootstrap';
 import './App.css';
+import React, { Component, useState,useEffect } from 'react';
+import axios from 'axios';
+import useAxios from 'axios-hooks';
 
-const App = ()=>{
 
+ 
+
+const myP = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
+
+function Axios() {
+  const [{ data, loading, error }, refetch] = useAxios(
+    'https://cars-demo-app.herokuapp.com/cars '
+  )
+ 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
   
+  return (
+    <div>
 
-  return(
-      <div>
-  <p>Hello</p>
-   </div>
+        {data.map(e => (<div>
+             
+              <p style={myP}>{e}</p>
+          </div>))}
+      <button onClick={refetch}>refetch</button>
+    </div>
   )
 }
 
-export default App;
+const App = ()=>{
+
+    const [list,setList] = React.useState([]);   
+
+    return(
+        <div>
+      
+     <Axios/>
+     </div>
+    )
+}
+
+
+
+
+export default App
