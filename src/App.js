4dca1051158eb22189fe4d490971cccb8283336d
kadirumac <kadirumac49@gmail.com>
2020-02-14 00:29:12 +0100

    andt

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 import React, { Component, useState,useEffect } from 'react';
 import axios from 'axios';
 import useAxios from 'axios-hooks';
+import HeaderSider from './components/HeaderSider';
 
-
- 
-
-const myP = {
+ const style = {
+  auto:{
+    width:"400px",
+    height:"auto",
+    margin:" auto"
+  },
+  img:{
+    width:"225px",
+    height:"149px",
+    marginLeft:"50px"
+  },
+  brand: {
     color: "white",
-    backgroundColor: "DodgerBlue",
+    backgroundColor: "#000",
+    opacity:"0.7",
     padding: "10px",
-    fontFamily: "Arial"
-  };
+    fontFamily: "Arial",
+    margin:"0 auto",
+    textAlign:"center",
+    borderRadius:"5px"
+  }
+
+ }
+
+
+
+
 
 function Axios() {
   const [{ data, loading, error }, refetch] = useAxios(
@@ -23,11 +42,11 @@ function Axios() {
   if (error) return <p>Error!</p>
   
   return (
-    <div>
+      <div style={style.auto}>
 
         {data.map(e => (<div>
-              <img src={e.imgUrl}></img>
-              <p style={myP}>{e.brand}</p>
+              <img style={style.img} src={e.imgUrl}></img>
+              <h3 style={e.brandColor}>{e.brand}</h3>
           </div>))}
       <button onClick={refetch}>refetch</button>
     </div>
@@ -40,7 +59,7 @@ const App = ()=>{
 
     return(
         <div>
-      
+      <HeaderSider/>
      <Axios/>
      </div>
     )
